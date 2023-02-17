dd129e8d28bb8aa66b0aebc0c3778cc0dab0130f
alejandrorojas2@gmail.com <alejandrorojas2@gmail.com>
2023-02-17 16:20:16 -0600

    adding genome db file and script

--- script.sh
+++ script.sh
@@ -3,4 +3,10 @@
 #SBATCH --output=ct_output.slurm
 #SBATCH --nodes=4
 #SBATCH --tasks-per-node=32
-$SBATCH --time=00:00:10
+#SBATCH --time=00:10:00
+
+module load blast/2.13.0+bin
+
+makeblastdb -i Sacc_genome.fna -dbtype nucl -parse_seqids -out Sacc_genome -title "Saccharomyces genome db"
+
+
